ce4ee08374995d5a58d311952645abd7599698ff
roberjunnior <roberjunior.dev@gmail.com>
2023-08-15 23:14:16 -0300

    Testando o git clone

--- teste1.js
+++ teste1.js
@@ -4,3 +4,4 @@ console.log(
 console.log("Testando após publicar");
 console.log("Teste de comando remote add origin");
 console.log("Testando o Git Pull");
+console.log("teste de git clone após apagar repositório do pc");
