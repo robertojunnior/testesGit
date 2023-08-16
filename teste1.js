0519acca9f0786c6932412e3c30059272c04475b
roberjunnior <roberjunior.dev@gmail.com>
2023-08-15 23:49:39 -0300

    teste branch

--- teste1.js
+++ teste1.js
@@ -5,3 +5,5 @@ console.log("Testando após publicar");
 console.log("Teste de comando remote add origin");
 console.log("Testando o Git Pull");
 console.log("teste de git clone após apagar repositório do pc");
+
+console.log("Teste branch testeBranch");
